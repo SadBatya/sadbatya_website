2ff49269e46c24b8b7dc4275e29d0db649b6e87c
sadbatya <sadbatya@mail.ru>
2025-09-25 16:18:16 +0200

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/widgets";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
