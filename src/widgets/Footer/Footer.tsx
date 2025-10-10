f4c69fb4cec21797b8fb16e594b3eddf5eeef126
sadbatya <sadbatya@mail.ru>
2025-10-10 21:30:28 +0200

    fix: footer text

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -3,15 +3,17 @@ import { externalPath } from "@/shared/routes";
 
 export const Footer = () => {
   const date = new Date();
-  
+
   return (
     <footer className="border-t border-[#2A2A2A] bg-[#121212] w-full">
       <div className="max-w-[1440px] w-[calc(100%-40px)] flex-col mx-auto gap-4 flex items-center py-8 justify-between">
         <div>
-          Designed and Development by
+          Дизайн и разработка
           <Link href={externalPath.github}> Sadbatya</Link>
         </div>
-        <p className="text-sm text-white/50">All right reserved {date.getFullYear()} </p>
+        <p className="text-sm text-white/50">
+          Все права защищены {date.getFullYear()} ©{" "}
+        </p>
       </div>
     </footer>
   );
