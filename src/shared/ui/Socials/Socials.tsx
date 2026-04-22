c4c7668d6ba3874ec7bd671d6aca2643d58ad8d9
sadbatya <sadbatya@mail.ru>
2026-04-22 13:40:06 +0200

    refactor: socials links

--- src/shared/ui/Socials/Socials.tsx
+++ src/shared/ui/Socials/Socials.tsx
@@ -3,18 +3,16 @@ import { Link } from "react-router";
 import { EXTERNAL_PATH } from "@/shared/routes";
 import { PiInstagramLogo, PiTelegramLogo, PiGithubLogo } from "react-icons/pi";
 
-export const Socials = () => {
-  return (
-    <div className={style.socials_icons}>
-      <Link to={EXTERNAL_PATH.instagram}>
-        <PiInstagramLogo className={style.social_icon} />
-      </Link>
-      <Link to={EXTERNAL_PATH.telegram}>
-        <PiTelegramLogo className={style.social_icon} />
-      </Link>
-      <Link to={EXTERNAL_PATH.github}>
-        <PiGithubLogo className={style.social_icon} />
-      </Link>
-    </div>
-  );
-};
+export const Socials = () => (
+  <div className={style.socials_icons}>
+    <Link to={EXTERNAL_PATH.instagram}>
+      <PiInstagramLogo className={style.social_icon} />
+    </Link>
+    <Link to={EXTERNAL_PATH.telegram}>
+      <PiTelegramLogo className={style.social_icon} />
+    </Link>
+    <Link to={EXTERNAL_PATH.github}>
+      <PiGithubLogo className={style.social_icon} />
+    </Link>
+  </div>
+);
